f8f5f0ee9132bdf24843f626ad380309a0357661
SeaDve <SeaDve@users.noreply.github.com>
2023-11-19 01:04:40 +0000

    deploy: 7214ff8215a24d53a6aa87cf85cc3d1ee5130179

--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"plotters_gtk4":[["impl&lt;OT: ObjectType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;OT&gt; for <a class=\"struct\" href=\"plotters_gtk4/struct.Paintable.html\" title=\"struct plotters_gtk4::Paintable\">Paintable</a>"]]
+"plotters_gtk4":[["impl&lt;OT: ObjectType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;OT&gt; for <a class=\"struct\" href=\"plotters_gtk4/struct.Paintable.html\" title=\"struct plotters_gtk4::Paintable\">Paintable</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
